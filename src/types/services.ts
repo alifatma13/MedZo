6c78939172bdb38c7dbfee72eaeb2189eea155f3
Fatma Ali <alifatma13@gmail.com>
2026-06-01 20:17:04 +1000

    Sanity Integration work

--- src/types/services.ts
+++ src/types/services.ts
@@ -13,23 +13,3 @@ export interface ServiceFeature {
   title: string;
   body: string;
 }
-
-export interface ServicePageContent {
-  slug: string;
-  metaTitle: string;
-  metaDescription: string;
-  heroLabel: string;
-  heroHeading: string;
-  heroIntro: string;
-  heroCtaLabel: string;
-  heroCtaHref: string;
-  heroImage: ImageMetadata;
-  featuresEyebrow: string;
-  featuresHeading: string;
-  featuresIntro: string;
-  features: ServiceFeature[];
-  ctaHeading: string;
-  ctaBody: string;
-  ctaLabel: string;
-  ctaHref: string;
-}
